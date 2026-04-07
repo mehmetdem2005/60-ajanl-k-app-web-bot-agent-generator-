7a87df6c3bebf1350d9e42a71f0510274801e246
mehmetdem2005 <mehmetdem2005@gmail.com>
2026-04-07 21:27:52 +0300

    chore: add migration placeholder

---
+++ scripts/migrate-to-core.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log('[migrate-to-core] Placeholder migration script.');
+console.log('[migrate-to-core] No legacy agents were found to migrate in this bootstrap snapshot.');
+process.exit(0);
